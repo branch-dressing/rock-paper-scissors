3998be23f6a648bc396ab8720f2b215152d9f668
joelpdurham <joeldur@gmail.com>
2019-10-02 20:59:15 -0700

    some style added

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 //pull all DOM elements
+const rock = document.getElementById('rocks');
+const previewSelection = rock.document.addEventListener('click', changeImage);
+
+function changeImage() {
+    
+}
 
 //set global vars
 
