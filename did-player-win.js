8b58ce29ec5e1c03de12816743042f9d4de0bf23
503-888-1044 Martell <drmartell@gmail.com>
2019-10-03 17:14:31 -0700

    switching drivers

--- did-player-win.js
+++ did-player-win.js
@@ -3,7 +3,7 @@ let wins = 0;
 let losses = 0;
 
 
-function didPlayerWin(playerThrow, compThrow) {
+export const getGameResults = (playerThrow, compThrow) => {
     if (playerThrow === compThrow) {
         tie ++;
         document.getElementById('ties').textContent = tie;
@@ -17,6 +17,16 @@ function didPlayerWin(playerThrow, compThrow) {
         document.getElementById('losses').textContent = losses;
         return 'Player Loses';
     }
-}
+};
 
-export default didPlayerWin ;
+export const resetAllStates = () => {
+    tie = 0;
+    wins = 0;
+    losses = 0;
+    
+    document.getElementById('computer-selection-img').src = 'https://tr.rbxcdn.com/d49a1ed85c55f18e053de3dc8a65c831/420/420/Decal/Png';
+    document.getElementById('player-selection-img').src = 'https://tr.rbxcdn.com/d49a1ed85c55f18e053de3dc8a65c831/420/420/Decal/Png';
+    document.getElementById('ties').textContent = tie;
+    document.getElementById('wins').textContent = wins;
+    document.getElementById('losses').textContent = losses;
+};
