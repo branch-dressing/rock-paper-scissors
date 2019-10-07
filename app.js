b731ed16cc3141436afdd9034dc94ddd3e3028c7
joelpdurham <joeldur@gmail.com>
2019-10-07 11:12:56 -0700

    added reset button and re-named a few functions

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 import { getThrowFromNumber } from './get-throw.js';
-import didPlayerWin from './did-player-win.js';
-import { changeToRock, changeToPaper, changeToScissors } from './change-images.js';
+import { getGameResults, resetAllStates } from './did-player-win.js';
+import { changeImageToRock, changeImageToPaper, changeImageToScissors } from './change-images.js';
 
-document.getElementById('rock-img').addEventListener('click', changeToRock);
-document.getElementById('paper-img').addEventListener('click', changeToPaper);
-document.getElementById('scissors-img').addEventListener('click', changeToScissors);
+document.getElementById('rock-img').addEventListener('click', changeImageToRock);
+document.getElementById('paper-img').addEventListener('click', changeImageToPaper);
+document.getElementById('scissors-img').addEventListener('click', changeImageToScissors);
 
 const fightButton = document.getElementById('fight');
+const resetButton = document.getElementById('reset-button');
 
 const rockPaperScissorsGo = () => {
     const playerSelection = document.querySelector('input:checked').value;
     const randomNumber = Math.floor(Math.random() * 3);
     const compSelection = getThrowFromNumber(randomNumber);
-    const outcome = didPlayerWin(playerSelection, compSelection);
+    const outcome = getGameResults(playerSelection, compSelection);
     document.getElementById('result').textContent = outcome;
 };
 
 fightButton.addEventListener('click', rockPaperScissorsGo);
+resetButton.addEventListener('click', resetAllStates);
--- did-player-win.js
+++ did-player-win.js
@@ -3,20 +3,39 @@ let wins = 0;
 let losses = 0;
 
 
-function didPlayerWin(playerThrow, compThrow) {
+export const getGameResults = (playerThrow, compThrow) => {
     if (playerThrow === compThrow) {
-        tie ++;
-        document.getElementById('ties').textContent = tie;
+        updateTies(++tie);
         return 'Tie';
     } else if ((playerThrow === 'Scissors' && compThrow === 'Paper') || (playerThrow === 'Rock' && compThrow === 'Scissors') || (playerThrow === 'Paper' && compThrow === 'Rock')) {
-        wins ++;
-        document.getElementById('wins').textContent = wins;
+        updateWins(++wins);
         return 'Player Wins';
     } else {
-        losses ++;
-        document.getElementById('losses').textContent = losses;
+        updateLosses(++losses);
         return 'Player Loses';
     }
-}
+};
 
-export default didPlayerWin ;
+export const resetAllStates = () => {
+    tie = 0;
+    wins = 0;
+    losses = 0;
+    
+    document.getElementById('computer-selection-img').src = 'https://tr.rbxcdn.com/d49a1ed85c55f18e053de3dc8a65c831/420/420/Decal/Png';
+    document.getElementById('player-selection-img').src = 'https://tr.rbxcdn.com/d49a1ed85c55f18e053de3dc8a65c831/420/420/Decal/Png';
+    updateTies(tie);
+    updateWins(wins);
+    updateLosses(losses);
+};
+
+const updateWins = (winsCount) => {
+    document.getElementById('wins').textContent = winsCount;
+};
+
+const updateLosses = (lossesCount) => {
+    document.getElementById('losses').textContent = lossesCount;
+};
+
+const updateTies = (tiesCount) => {
+    document.getElementById('ties').textContent = tiesCount;
+};
